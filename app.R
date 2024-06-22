779b87e2b58ecdcef4ad8ab99c2903650b16a81f
ezekiel-kaeyros <ezekiel.dombissi@kaeyros-analytics.com>
2024-06-22 12:13:06 +0100

    pusj

--- app.R
+++ app.R
@@ -4,5 +4,5 @@ library(plumber)
 r <- plumb("plumber.R")
 
 # Démarrer le serveur sur le port 8000 (ou tout autre port de votre choix)
-r$run(port = 8000)
+r$run(host = "0.0.0.0", port = 8000)
 #plumb(file='plumber.R')$run(port = 8000)
